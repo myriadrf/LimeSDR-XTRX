2c66f812757934b946f5391541a707f6868024eb
Andrew Back <andrew@abopen.com>
2025-11-10 16:07:23 +0000

    Update docs to latest Sphinx config.

--- docs/conf.py
+++ docs/conf.py
@@ -4,46 +4,118 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
+import sys
 import sphinx_rtd_theme
+import requests
+import warnings
+from pathlib import Path
+from urllib.parse import urljoin
 
-# -- Project information -----------------------------------------------------
+# -- Path setup --------------------------------------------------------------
+# Put config directory first on sys.path so imports from this work
 
-project = 'LimeSDR XTRX Boards'
-copyright = '2024, MyriadRF Contributors'
-author = 'Lime Microsystems'
+HERE = Path(__file__).resolve().parent
+sys.path.insert(0, str(HERE))
 
-# The full version, including alpha/beta/rc tags
-release = '23.01'
+# -- MyriadRF configuration --------------------------------------------------
 
-highlight_language = 'console'
+# Path to remote assets
+asset_base = 'https://assets.myriadrf.net/sphinx/'
+
+# Import the project config from project.py
+import project as project_cfg
+
+# Assign project config variables to local names
+project = project_cfg.project 
+copyright = project_cfg.copyright
+author  = project_cfg.author
+release = project_cfg.release
+
+# Set the CSS to use
+if not project_cfg.archived:
+    html_css_files = [urljoin(asset_base, 'mr-custom.css')]
+else:
+    html_css_files = [urljoin(asset_base, 'mr-archived.css')]
+
+# Retrieve remote asset
+def _fetch_remote(path, base=asset_base, timeout=10, default=""):
+    url = urljoin(base, path)
+    try:
+        resp = requests.get(url, timeout=timeout)
+        resp.raise_for_status()
+        return resp.text
+    except requests.RequestException as exc:
+        warnings.warn(f"Failed to fetch {url}: {exc}")
+        return default
+    
+# Read local asset 
+def _read_local(path: Path) -> str:
+    try:
+        return path.read_text(encoding='utf-8')
+    except Exception:
+        return ""
+
+# Get remote custom footer and navbar and
+# global substitutions and external links
+extrabody_content = _fetch_remote('mr-navbar.html')
+extrafooter_content = _fetch_remote('mr-footer.html')
+global_subs = _fetch_remote('substitutions.conf')
+global_extlinks = _fetch_remote('extlinks.conf')
+
+# Get local substitutions and external links
+local_subs = _read_local(HERE / 'substitutions.conf')
+local_extlinks = _read_local(HERE / 'extlinks.conf')
+
+# HTML customisation 
+html_context = {
+    'extrabody': extrabody_content,
+    'extrafooter': extrafooter_content,
+    'display_github': True,
+    'github_user': 'myriadrf',
+    'github_repo': project_cfg.github_repo,
+    'github_version': project_cfg.github_repo_path,
+    'archived': project_cfg.archived
+}
+
+# Merge remote + local RST epilog content
+parts = []
+
+if global_subs:
+    parts.append(global_subs.rstrip() + "\n")
+if local_subs:
+    parts.append(local_subs.rstrip() + "\n")
+if global_extlinks:
+    parts.append(global_extlinks.rstrip() + "\n")
+if local_extlinks:
+    parts.append(local_extlinks.rstrip() + "\n")
+
+if parts:
+    rst_epilog = "\n".join(parts)
+else:
+    # Fallback: if nothing was found/read, keep original include-style epilog 
+    # so that Sphinx can try to include files by path.
+    rst_epilog = """
+.. include:: /substitutions.conf
+.. include:: /extlinks.conf
+"""
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-
+# Sphinx extensions
 extensions = [
     'sphinx.ext.autosectionlabel',
     'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
     'sphinx-mathjax-offline',
+    'sphinx_code_tabs',
     'sphinx_rtd_theme',
-    'sphinx_tabs.tabs',
-    'sphinx_inline_tabs'
+    'notfound.extension'
 ]
 
-# Add any paths that contain templates here, relative to this directory.
+# Allow same section headings and thus labels to be used across documents.
+autosectionlabel_prefix_document = True
+
+# Paths that contain templates
 templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
@@ -51,59 +123,21 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['venv', '_build', 'Thumbs.db', '.DS_Store']
 
-html_static_path = ['_static']
-
-html_css_files = ['mr-custom.css']
-
-html_last_updated_fmt = '%b %d, %Y'
-
-html_context = {
-    'display_github': True,
-    'github_user': 'myriadrf',
-    'github_repo': 'LimeSDR-XTRX',
-    'github_version': 'master/docs/' 
-}
-
-# This is where we place substitutions, such as for Unicode characters.
-rst_epilog = """
-.. include:: /substitutions.txt
-"""
-
-# Allow same section headings and thus labels to be used across documents.
-autosectionlabel_prefix_document = True
-
-# Generate SVG format images from LaTeX math embedded in RST documents.
-#imgmath_image_format = 'svg'
-
-# Default size of 12pt for equations is a bit small.
-#imgmath_font_size = 16
-
-#mathjax_path = 'https://cdn.jsdelivr.net/npm/mathjax@2/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
-
 # -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 
 html_theme = "sphinx_rtd_theme"
 
 html_theme_options = {
     'logo_only': False,
-    'display_version': True,
     'collapse_navigation': False,
     'sticky_navigation': True,
-    'navigation_depth': 4
+    'navigation_depth': 5
 }
-
-#html_logo = 'images/LimeADPD_logo_200w.png'
-#html_favicon = 'images/LimeADPD_favicon_34x34.png'
 
 html_show_sphinx = False
 
+# HTML last updated formatting
+html_last_updated_fmt = '%b %d, %Y'
+
 root_doc = 'index'
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
